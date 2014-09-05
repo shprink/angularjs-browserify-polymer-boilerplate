b3d67606ad0011469bf8449c216789fcd488ff07
shprink <julien.renaux@gmail.com>
2014-09-05 18:32:15 +0200

    bundle

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp')
         , concat = require('gulp-concat')
         , sass = require("gulp-sass")
         , minifyCss = require("gulp-minify-css")
+        , ignore = require('gulp-ignore')
         , minifyHtml = require("gulp-minify-html")
         , stripDebug = require('gulp-strip-debug')
         , browserify = require('gulp-browserify')
@@ -52,14 +53,70 @@ gulp.task('browserify', function() {
 });
 
 gulp.task('css', function() {
-    merge(gulp.src([pathService.get('bower') + 'angular-material/angular-material.css'])
-            , gulp.src(['lib/scss/**/*.scss']).pipe(sass()))
+    //merge(gulp.src([pathService.get('bower') + 'angular-material/angular-material.css'])
+    gulp.src(['lib/scss/**/*.scss']).pipe(sass())
             .pipe(concat(IS_RELEASE_BUILD ? 'bundle.min.css' : 'bundle.css'))
             .pipe(gulpif(IS_RELEASE_BUILD, minifyCss()))
             .pipe(gulp.dest('./www/css'));
 });
 
 gulp.task('views', function() {
+
+//    gulp.src(['polymer/**/*.*'
+//                , 'core-ajax/**/*.*'
+//                , 'core-animation/**/*.*'
+//                , 'web-animations-js/**/*.*'
+//                , 'core-animated-pages/**/*.*'
+//                , 'core-collapse/**/*.*'
+//                , 'core-drag-drop/**/*.*'
+//                , 'core-drawer-panel/**/*.*'
+//                , 'core-dropdown/**/*.*'
+//                , 'core-field/**/*.*'
+//                , 'core-header-panel/**/*.*'
+//                , 'core-icon-button/**/*.*'
+//                , 'core-icons/**/*.*'
+//                , 'core-input/**/*.*'
+//                , 'core-item/**/*.*'
+//                , 'core-layout/**/*.*'
+//                , 'core-layout-grid/**/*.*'
+//                , 'core-layout-trbl/**/*.*'
+//                , 'core-list/**/*.*'
+//                , 'core-localstorage/**/*.*'
+//                , 'core-media-query/**/*.*'
+//                , 'core-menu/**/*.*'
+//                , 'core-menu/**/*.*'
+//                , 'core-menu-button/**/*.*'
+//                , 'core-meta/**/*.*'
+//                , 'core-overlay/**/*.*'
+//                , 'core-pages/**/*.*'
+//                , 'core-range/**/*.*'
+//                , 'core-scaffold/**/*.*'
+//                , 'core-scroll-header-panel/**/*.*'
+//                , 'core-selection/**/*.*'
+//                , 'core-selector/**/*.*'
+//                , 'core-shared-lib/**/*.*'
+//                , 'core-signals/**/*.*'
+//                , 'core-splitter/**/*.*'
+//                , 'core-style/**/*.*'
+//                , 'core-toolbar/**/*.*'
+//                , 'core-tooltip/**/*.*'
+//                , 'core-transition/**/*.*'
+//                , 'core-icon*/**/*.*'
+//                , 'core-iconset/**/*.*'
+//                , 'core-iconset-svg/**/*.*'
+//                , 'context-free-parser/**/*.*'
+//                , 'marked/**/*.*'
+//                , 'marked-element/**/*.*'
+//                , 'paper-*/**/*.*'
+//    ], {cwd: pathService.get('bower') + '/**'})
+//            .pipe(ignore.exclude(['**/**.md', '**/bower.json', '**/test/**', '**/demos/**']))
+//            .pipe(gulpif(IS_RELEASE_BUILD, minifyHtml({
+//                quotes: true,
+//                empty: true
+//            })))
+//            .pipe(gulp.dest('./www/views/polymer/'));
+
+
     gulp.src(['lib/index.html'])
             .pipe(gulpif(IS_RELEASE_BUILD, minifyHtml({
                 quotes: true,
