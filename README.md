571b1c21b043b7c72be23f6de8493921fe00c137
shprink <julien.renaux@gmail.com>
2014-10-23 16:13:46 +0200

    simplifying polymer by using vulcanize

---
+++ README.md
@@ -0,0 +1,40 @@
+# Installation
+
+```
+git clone git@github.com:shprink/angularjs-browserify-polymer-boilerplate.git
+cd angularjs-browserify-polymer-boilerplate
+```
+
+## NodeJs
+
+Need to have a working nodejs server
+
+http://julienrenaux.fr/2013/05/16/how-to-install-node-js-coffeescript-less-and-uglify-js-on-ubuntu/
+
+## Npm dependencies
+
+```
+sudo npm install
+sudo npm install -g bower
+sudo npm install -g gulp
+```
+
+## Bower
+
+```
+bower install
+```
+
+# Build
+
+```
+gulp 
+```
+
+# Watch & Live reload (dev mode)
+
+```
+gulp watch
+```
+
+Then open ```http://localhost:5000/```
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,27 @@
 var gulp = require('gulp'),
-    requireDir = require('require-dir'),
-    dir = requireDir('./gulp'),
-    argv = require('minimist')(process.argv.slice(2)),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    sass = require("gulp-sass"),
-    minifyCss = require("gulp-minify-css"),
-    ignore = require('gulp-ignore'),
-    minifyHtml = require("gulp-minify-html"),
-    stripDebug = require('gulp-strip-debug'),
-    browserify = require('gulp-browserify'),
-    gulpif = require('gulp-if'),
-    merge = require('merge-stream'),
-    taskListing = require('gulp-task-listing');
+        requireDir = require('require-dir'),
+        dir = requireDir('./gulp'),
+        argv = require('minimist')(process.argv.slice(2)),
+        uglify = require('gulp-uglify'),
+        concat = require('gulp-concat'),
+        sass = require("gulp-sass"),
+        minifyCss = require("gulp-minify-css"),
+        ignore = require('gulp-ignore'),
+        minifyHtml = require("gulp-minify-html"),
+        stripDebug = require('gulp-strip-debug'),
+        browserify = require('gulp-browserify'),
+        gulpif = require('gulp-if'),
+        merge = require('merge-stream'),
+        vulcanize = require('gulp-vulcanize'),
+        taskListing = require('gulp-task-listing');
 
 
 var express = require('express'),
-    refresh = require('gulp-livereload'),
-    livereload = require('connect-livereload'),
-    livereloadport = 35729,
-    serverport = 5000,
-    server = express();
+        refresh = require('gulp-livereload'),
+        livereload = require('connect-livereload'),
+        livereloadport = 35729,
+        serverport = 5000,
+        server = express();
 
 server.use(livereload({
     port: livereloadport
@@ -32,92 +33,50 @@ var IS_POLYMER_INCLUDED = (typeof argv.polymer === 'undefined') ? false : true;
 
 var pathService = require('./gulp/services/path');
 
-gulp.task('default', ['browserify', 'views', 'css']);
+gulp.task('default', ['browserify', 'views', 'polymer', 'css']);
 
 gulp.task('browserify', function() {
     gulp.src('lib/js/main.js')
-        .pipe(browserify({
-            insertGlobals: false,
-            debug: !IS_RELEASE_BUILD
-        }))
-        .pipe(gulpif(IS_RELEASE_BUILD, stripDebug()))
-        .pipe(concat('bundle.js'))
-        .pipe(gulpif(IS_RELEASE_BUILD, uglify({
-            mangle: false
-        })))
-        .pipe(gulpif(!IS_RELEASE_BUILD, uglify({ // removing comments to reduce file size
-            mangle: false,
-            compress: false,
-            output: {
-                beautify: true
-            }
-        })))
-        .pipe(gulp.dest('./www/js/'));
+            .pipe(browserify({
+                insertGlobals: false,
+                debug: !IS_RELEASE_BUILD
+            }))
+            .pipe(gulpif(IS_RELEASE_BUILD, stripDebug()))
+            .pipe(concat('bundle.js'))
+            .pipe(gulpif(IS_RELEASE_BUILD, uglify({
+                mangle: false
+            })))
+            .pipe(gulpif(!IS_RELEASE_BUILD, uglify({// removing comments to reduce file size
+                mangle: false,
+                compress: false,
+                output: {
+                    beautify: true
+                }
+            })))
+            .pipe(gulp.dest('./www/js/'));
 });
 
 gulp.task('css', function() {
     //merge(gulp.src([pathService.get('bower') + 'angular-material/angular-material.css'])
     gulp.src(['lib/scss/**/*.scss']).pipe(sass())
-        .pipe(concat(IS_RELEASE_BUILD ? 'bundle.min.css' : 'bundle.css'))
-        .pipe(gulpif(IS_RELEASE_BUILD, minifyCss()))
-        .pipe(gulp.dest('./www/css'));
+            .pipe(concat(IS_RELEASE_BUILD ? 'bundle.min.css' : 'bundle.css'))
+            .pipe(gulpif(IS_RELEASE_BUILD, minifyCss()))
+            .pipe(gulp.dest('./www/css'));
 });
 
 gulp.task('polymer', function() {
-    gulp.src(['polymer/**/*.*'
-        //    , 'core-ajax/**/*.*'
-        //    , 'core-animation/**/*.*'
-        //    , 'web-animations-js/**/*.*'
-        //    , 'core-animated-pages/**/*.*'
-        , 'core-collapse/**/*.*'
-        //    , 'core-drag-drop/**/*.*'
-        , 'core-drawer-panel/**/*.*'
-        //    , 'core-dropdown/**/*.*'
-        //    , 'core-field/**/*.*'
-        , 'core-header-panel/**/*.*', 'core-icon-button/**/*.*', 'core-icons/**/*.*', 'core-input/**/*.*',
-        'core-item/**/*.*'
-        //    , 'core-layout/**/*.*'
-        //    , 'core-layout-grid/**/*.*'
-        //    , 'core-layout-trbl/**/*.*'
-        , 'core-list/**/*.*'
-        //    , 'core-localstorage/**/*.*'
-        , 'core-media-query/**/*.*'
-        //    , 'core-menu/**/*.*'
-        , 'core-menu/**/*.*'
-        //    , 'core-menu-button/**/*.*'
-        , 'core-meta/**/*.*'
-        //    , 'core-overlay/**/*.*'
-        , 'core-pages/**/*.*'
-        //    , 'core-range/**/*.*'
-        //    , 'core-scaffold/**/*.*'
-        //    , 'core-scroll-header-panel/**/*.*'
-        , 'core-selection/**/*.*', 'core-selector/**/*.*'
-        //    , 'core-shared-lib/**/*.*'
-        //    , 'core-signals/**/*.*'
-        //    , 'core-splitter/**/*.*'
-        , 'core-style/**/*.*', 'core-toolbar/**/*.*'
-        //    , 'core-tooltip/**/*.*'
-        //    , 'core-transition/**/*.*'
-        //    , 'core-icon*/**/*.*'
-        //    , 'core-iconset/**/*.*'
-        //    , 'core-iconset-svg/**/*.*'
-        //    , 'context-free-parser/**/*.*'
-        //    , 'marked/**/*.*'
-        //    , 'marked-element/**/*.*'
-        //    , 'paper-*/**/*.*'
-        , 'paper-input/**/*.*', 'paper-button/**/*.*', 'paper-shadow/**/*.*', 'paper-ripple/**/*.*',
-        'paper-focusable/**/*.*'
-    ], {
-        cwd: pathService.get('bower') + '/**'
-    })
-        .pipe(ignore.exclude(['**/**.md', '**/bower.json', '**/test/**', '**/demos/**', 'demo.html',
-            'metadata.html'
-        ]))
-        .pipe(gulpif(IS_RELEASE_BUILD, minifyHtml({
-            quotes: true,
-            empty: true
-        })))
-        .pipe(gulp.dest('./www/views/polymer/'));
+    return gulp.src('lib/views/Polymer/bootstrap.html')
+            .pipe(vulcanize({
+                dest: './www/views/Polymer/',
+                inline: true,
+                csp: true,
+                strip: true
+            }))
+//            .pipe(minifyHtml({
+//                quotes: true,
+//                empty: true
+//            }))
+            .pipe(gulp.dest('./www/views/Polymer/'));
 });
 
 gulp.task('views', function() {
@@ -127,18 +86,18 @@ gulp.task('views', function() {
     }
 
     gulp.src(['lib/index.html'])
-        .pipe(gulpif(IS_RELEASE_BUILD, minifyHtml({
-            quotes: true,
-            empty: true
-        })))
-        .pipe(gulp.dest('./www'));
+            .pipe(gulpif(IS_RELEASE_BUILD, minifyHtml({
+                quotes: true,
+                empty: true
+            })))
+            .pipe(gulp.dest('./www'));
 
     gulp.src(['lib/views/**/*.html'])
-        .pipe(gulpif(IS_RELEASE_BUILD, minifyHtml({
-            quotes: true,
-            empty: true
-        })))
-        .pipe(gulp.dest('./www/views'));
+            .pipe(gulpif(IS_RELEASE_BUILD, minifyHtml({
+                quotes: true,
+                empty: true
+            })))
+            .pipe(gulp.dest('./www/views'));
 });
 
 gulp.task('watch', [], function() {
@@ -157,7 +116,8 @@ gulp.task('watch', [], function() {
     ]);
 
     gulp.watch(['lib/views/**/*.html'], [
-        'views'
+        'views',
+        'polymer'
     ]);
 
     gulp.watch('./www/**').on('change', refresh.changed);
